d16669a6a029657d99070e7297f366c328130250
Windsongstudio <windsongstudio@protonmail.com>
2026-09-10 00:12:40 +0200

    Add script to include header from external HTML

---
+++ script/include-header.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const headerContainer = document.getElementById("site-header");
+  if (headerContainer) {
+    fetch("/header.html")
+      .then(response => response.text())
+      .then(html => {
+        headerContainer.innerHTML = html;
+      })
+      .catch(err => console.error("Header kon niet worden geladen:", err));
+  }
+});
