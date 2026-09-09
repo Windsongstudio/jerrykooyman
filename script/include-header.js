066fbe4a4a6d59bbe76ff76af8176f0cb008a5d7
Windsongstudio <windsongstudio@protonmail.com>
2026-09-10 00:48:09 +0200

    Change fetch path for header to relative path

--- script/include-header.js
+++ script/include-header.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const headerContainer = document.getElementById("site-header");
   if (headerContainer) {
-    fetch("/header.html")
-      .then(response => response.text())
+    // Relatief pad gebruiken (zonder / vooraan)
+    fetch("header.html")
+      .then(response => {
+        if (!response.ok) throw new Error("Netwerkfout bij laden header");
+        return response.text();
+      })
       .then(html => {
         headerContainer.innerHTML = html;
       })
